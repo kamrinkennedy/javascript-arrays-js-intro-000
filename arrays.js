710a7cad33b54c19b0506f0678261e1e75d3333e
Kamrin Kennedy <kamrinkennedy@gmail.com>
2018-10-04 06:40:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,13 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = [
+  'snickers', 
+  'hundred grand', 
+  'kitkat', 
+  'skittles'
+]
+
+function addElementToBeginningOfArray(array, element) {
+  var pooDiddly = [...array, element];
+  return pooDiddly;
+}
+
+
