b6ddc781e187b5109d60748ab2d32101b656419f
Kamrin Kennedy <kamrinkennedy@gmail.com>
2018-10-04 07:08:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,6 @@ function accessElementInArray (array, index) {
 };
 
 function destructivelyRemoveElementFromBeginningOfArray (array) {
-  var alteredArray2 = array.shift();
+  array.shift();
   return array
 }
